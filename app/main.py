e58301c6faa417822fff3159a1812487afc94c7c
mops123 <16187961+mops123@users.noreply.github.com>
2025-06-29 09:14:19 +0200

    rewriting main.py to clean logs

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 import asyncio
-from aiogram.utils import executor
 from app.bot import dp
 from app.webserver import run_webserver
 from app.handlers import new_members, commands
@@ -8,7 +7,12 @@ from app.handlers import new_members, commands
 new_members.register_handlers(dp)
 commands.register(dp)
 
+async def main():
+    # Запускаємо webserver і polling паралельно
+    await asyncio.gather(
+        run_webserver(),
+        dp.start_polling()
+    )
+
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.create_task(run_webserver())
-    executor.start_polling(dp, skip_updates=True)
+    asyncio.run(main())
